be7380ae9f6793fa45baf5c81c55e0a2d5bdde2f
Zarbar <zakaria.nouara@gmail.com>
2026-04-10 17:01:49 +0200

    US01 - README + initialisation complète

--- backend/src/main/java/com/finalspace/backend/controller/HealthController.java
+++ backend/src/main/java/com/finalspace/backend/controller/HealthController.java
@@ -1,13 +1,16 @@
 package com.finalspace.backend.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 public class HealthController {
 
     @GetMapping("/api/health")
     public String health() {
+        log.info("Health check endpoint called");
         return "OK";
     }
 }
